d194c5e46d00cc42b6b20e9335538ed07b5c8db6
Lev Vorobev <60810299+thylaakari@users.noreply.github.com>
2025-08-08 08:35:13 +0500

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,8 @@ export default defineNuxtConfig({
   modules: ['@nuxt/ui', '@nuxtjs/supabase'],
   css: ['~/assets/css/main.css'],
   supabase: {
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRnaWl2ZHNuenNsb2d0YmJycWhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ1NzczNzQsImV4cCI6MjA3MDE1MzM3NH0.56Zcg-1RrsNRWYfXkSxvREbUVqQ0DljprwjFDdibjgQ',
+    url: 'https://dgiivdsnzslogtbbrqho.supabase.co',
     redirect: false,
   },
 })
